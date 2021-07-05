edc442e7cf930e87d83e7dc379d0e529fa61c200
kasutinai <ak9mail@mail.ru>
2021-07-05 18:51:43 +0700

    05.07.2021

---
+++ DB/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DB
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            var db = new DBConnector();
+
+            db.ShowAllTables();
+
+            Console.WriteLine(db.Add());            
+
+            db.ShowAllTables();
+
+            Console.ReadLine();
+        }        
+    }
+}
