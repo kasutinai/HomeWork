bfa218ab877e4e7ab372a1d193cd27e0c19a7f30
kasutinai <ak9mail@mail.ru>
2021-07-14 20:18:31 +0700

    14.07.2021

---
+++ DelegatesAndActions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using DocReceiver;
+
+namespace DelegatesAndActions
+{    
+    class Program
+    {
+        private static bool _close = false;
+        static void Main(string[] args)
+        {
+            try
+            {
+                string targetDirectory = "C:\\Passport";
+                List<string> fileList = new() { "Паспорт.jpg", "Заявление.txt", "Фото.jpg" };
+                int waitingInterval = 10000;
+                DocumentsReceiver doc = new();
+
+                doc.DocumentsReady += Ready;
+                doc.TimedOut += TimedOut;
+
+                doc.Start(targetDirectory, waitingInterval, fileList);
+
+                while (!_close)
+                {
+                }
+
+                System.Threading.Thread.Sleep(1000);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void TimedOut()
+        {
+            Console.WriteLine("Time is out.");
+            _close = true;
+        }
+
+        private static void Ready()
+        {
+            Console.WriteLine("Documents are ready.");
+            _close = true;
+        }
+    }
+}
---
+++ DocumentsReceiver/DocumentsReceiver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Timers;
+
+namespace DocReceiver
+{
+    public class DocumentsReceiver
+    {
+        public event Action DocumentsReady;
+        public event Action TimedOut;
+
+        private FileSystemWatcher _watcher;
+        private Timer _timer = new Timer();
+
+        private string _targetDirectory;
+        private List<string> _fileList;
+
+        public void Start(string targetDirectory, int waitingInterval, List<string> fileList)
+        {
+            var dir = new DirectoryInfo(targetDirectory);
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Directory {targetDirectory} not found");
+
+            _targetDirectory = targetDirectory;
+            _fileList = fileList;
+            _watcher = new FileSystemWatcher(targetDirectory);
+
+            _fileList.ForEach(file => _watcher.Filters.Add(file));
+
+            _watcher.Created += CheckFiles;
+            _watcher.Deleted += CheckFiles;
+            _watcher.Renamed += CheckFiles;
+
+            _watcher.EnableRaisingEvents = true;
+
+            _timer.Interval = waitingInterval;
+            _timer.Elapsed += IntervalElapsed;
+            _timer.Start();
+        }
+
+        private void IntervalElapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispose();
+            TimedOut?.Invoke();
+        }
+
+        private void CheckFiles(object sender, FileSystemEventArgs e)
+        {
+            int i = 0;
+            var dir = new DirectoryInfo(_targetDirectory);
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                if (_fileList.Contains(file.Name))
+                    i++;
+            }
+
+            if (i == _fileList.Count)
+            {
+                Dispose();
+                DocumentsReady?.Invoke();
+            }
+        }
+
+        private void Dispose()
+        {
+            _watcher.Dispose();
+            _timer.Dispose();
+        }
+    }
+}
